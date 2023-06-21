cb720e5672aca073214a7afd65d2e03a6e226c11
Thilina Rathnayake <thilinarmtb@gmail.com>
2023-06-21 15:10:36 -0400

    Update scripts, move plot.py to scripts/

--- scripts/frontier.sh
+++ scripts/frontier.sh
@@ -5,20 +5,11 @@
 : ${QUEUE:="batch"}
 : ${OCCA_VERBOSE:=1}
 
+### Don't touch anything that follows this line. ###
 if [ $# -ne 3 ]; then
   echo "Usage: [PROJ_ID=value] $0 <backend> <# compute nodes> <hh:mm:ss>"
   exit 0
 fi
-
-backend=$1
-nodes=$2
-time=$3
-
-bin=${OMK_INSTALL_DIR}/bin/driver
-gpu_per_node=1
-core_per_node=1
-let nn=$nodes*$gpu_per_node
-let ntasks=nn
 
 if [ -z "$PROJ_ID" ]; then
   echo "ERROR: PROJ_ID is empty"
@@ -30,10 +21,25 @@ if [ -z "$QUEUE" ]; then
   exit 1
 fi
 
+if [ -z "$OMK_INSTALL_DIR" ]; then
+  echo "ERROR: OMK_INSTALL_DIR is empty"
+  exit 1
+fi
+
+bin=${OMK_INSTALL_DIR}/bin/driver
 if [ ! -f $bin ]; then
   echo "Cannot find" $bin
   exit 1
 fi
+
+backend=$1
+nodes=$2
+time=$3
+
+gpu_per_node=1
+core_per_node=1
+let nn=$nodes*$gpu_per_node
+let ntasks=nn
 
 time_fmt=`echo $time|tr ":" " "|awk '{print NF}'`
 if [ "$time_fmt" -ne "3" ]; then
@@ -70,6 +76,7 @@ echo "#SBATCH --ntasks-per-node=$gpu_per_node" >>$SFILE
 echo "#SBATCH --gpus-per-task=1" >>$SFILE
 echo "#SBATCH --gpu-bind=closest" >>$SFILE
 echo "#SBATCH --cpus-per-task=$core_per_node" >>$SFILE
+echo "" >>$SFILE
 
 echo "module load PrgEnv-gnu" >> $SFILE
 echo "module load craype-accel-amd-gfx90a" >> $SFILE
@@ -77,28 +84,33 @@ echo "module load cray-mpich" >> $SFILE
 echo "module load rocm" >> $SFILE
 echo "module unload cray-libsci" >> $SFILE
 echo "module list" >> $SFILE
+echo "" >>$SFILE
 
 echo "rocm-smi" >>$SFILE
 echo "rocm-smi --showpids" >>$SFILE
+echo "" >>$SFILE
 
 echo "# which nodes am I running on?" >>$SFILE
 echo "squeue -u \$USER" >>$SFILE
+echo "" >>$SFILE
+
+echo "export OMK_INSTALL_DIR=${OMK_INSTALL_DIR}" >>$SFILE
+echo "" >>$SFILE
 
 echo "export MPICH_GPU_SUPPORT_ENABLED=1" >>$SFILE
+echo "export MPICH_OFI_NIC_POLICY=NUMA" >>$SFILE
+echo "" >>$SFILE
 
 ## These must be set before compiling so the executable picks up GTL
 echo "export PE_MPICH_GTL_DIR_amd_gfx90a=\"-L${CRAY_MPICH_ROOTDIR}/gtl/lib\"" \
   >> $SFILE
 echo "export PE_MPICH_GTL_LIBS_amd_gfx90a=\"-lmpi_gtl_hsa\"" >> $SFILE
+echo "" >> $SFILE
 
 echo "ulimit -s unlimited " >>$SFILE
-
-echo "export MPICH_OFI_NIC_POLICY=NUMA" >>$SFILE
-
 echo "" >> $SFILE
+
 echo "date" >>$SFILE
-echo "" >> $SFILE
-
 echo "" >> $SFILE
 
 echo "srun -N $nodes -n $ntasks $bin --backend ${backend}" >>$SFILE
--- scripts/plot.py
+++ scripts/plot.py
@@ -37,7 +37,7 @@ def plot_h2d_d2h(A: np.array) -> None:
 def plot_d2d(A: np.array) -> None:
     fig = plt.figure(figsize=(9, 9))
 
-    block_size = np.unique(A[:, 1].astype(int))
+    block_size = np.unique(A[:, 0].astype(int))
     for bsize in block_size:
         B = A[A[:, 0].astype(int) == bsize]
         words = B[:, 1].astype(int)
@@ -82,15 +82,44 @@ def plot_variants(A: np.array, title: str) -> None:
         plt.close()
 
 
+def plot_ratio(A: np.array) -> None:
+    kernels = np.unique(A[:, 0])
+    assert len(kernels) == 2
+
+    fig = plt.figure(figsize=(9, 9))
+    B = A[A[:, 0] == kernels[0]]
+    C = A[A[:, 0] == kernels[1]]
+
+    block_size = np.unique(A[:, 1].astype(int))
+    for bsize in block_size:
+        B1 = B[B[:, 1].astype(int) == bsize]
+        C1 = C[C[:, 1].astype(int) == bsize]
+        words = B1[:, 2].astype(int)
+        time_b = B1[:, 3].astype(float)
+        time_c = C1[:, 3].astype(float)
+        plt.plot(words, np.divide(time_b, time_c), label=f"block-size={bsize}")
+
+    plt.title(f"Ratio: {kernels[0]}/{kernels[1]}", fontsize=23)
+    plt.xlabel("# of 8-byte Words", fontsize=18)
+    plt.ylabel("Ratio", fontsize=18)
+    plt.xscale("log", base=10)
+    plt.xticks(fontsize=16)
+    plt.yticks(fontsize=16)
+    plt.legend(loc=0)
+    plt.savefig(f"ratio.pdf")
+    plt.close()
+
+
 if __name__ == "__main__":
     A = read("data/frontier/omk_h2d_d2h.txt")
-    plot_h2d_d2h(A)
-
     B = read("data/frontier/omk_d2d.txt")
-    plot_d2d(B)
-
     C = read("data/frontier/omk_daxpy.txt")
-    plot_variants(C, "Daxpy")
-
     D = read("data/frontier/omk_reduction.txt")
-    plot_variants(C, "Reduction")
+    E = read("data/frontier/omk_scalar_mul_div.txt")
+
+    plot_h2d_d2h(A)
+    plot_d2d(B)
+    plot_variants(C, "Daxpy")
+    plot_variants(D, "Reduction")
+    plot_variants(E, "Scale")
+    plot_ratio(E)
